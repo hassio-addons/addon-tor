5da18f8716b23461a2057e8a27d92cfc825ba47d
ludeeus <joasoe@gmail.com>
2018-09-01 17:35:08 +0200

    removed whitespace

--- tor/rootfs/etc/cont-init.d/90-hostname.sh
+++ tor/rootfs/etc/cont-init.d/90-hostname.sh
@@ -13,11 +13,11 @@ if hass.config.true 'hidden_services'; then
     hass.log.debug 'Starting Tor temporarly...'
 
     exec 3< <(tor)
-    
+
     until hass.file_exists "${hostname_file}"; do
         sleep 1
     done
-    
+
     grep -m 1 "Bootstrapped 100%: Done" <&3 >/dev/null 2>&1
 
     kill "$(pgrep tor)" >/dev/null 2>&1
