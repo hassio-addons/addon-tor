859a3b2f70b21ea78609ad49562b713f096499d2
Psykopear <psykopear@gmail.com>
2022-06-10 21:47:42 +0200

    🚑 Fix Hidden Service V3 Client Authorization (#105)
    Co-authored-by: GuillaumeHubR <77720768+GuillaumeHubR@users.noreply.github.com>
    Co-authored-by: Franck Nijhof <git@frenck.dev>

--- tor/rootfs/etc/cont-init.d/20-dirs.sh
+++ tor/rootfs/etc/cont-init.d/20-dirs.sh
@@ -3,8 +3,9 @@
 # Home Assistant Community Add-on: Tor
 # Ensures necessary directories exists
 # ==============================================================================
-if ! bashio::fs.directory_exists '/ssl/tor'; then
-    mkdir -p /ssl/tor/hidden_service \
-        || bashio::exit.nok 'Could not create tor data directory'
-    chmod -R 0700 /ssl/tor
-fi
+mkdir -p \
+    /ssl/tor/hidden_service/authorized_clients \
+    /ssl/tor/hidden_service/clients \
+    || bashio::exit.nok 'Could not create tor data directory'
+
+chmod -R 0700 /ssl/tor
--- tor/rootfs/etc/cont-init.d/52-tor-services.sh
+++ tor/rootfs/etc/cont-init.d/52-tor-services.sh
@@ -3,17 +3,24 @@
 # Home Assistant Community Add-on: Tor
 # Configures the hidden services
 # ==============================================================================
-declare virtual_port
-declare target_port
-declare port
-declare host
 declare clientname
+declare host
+declare port
+declare key
+declare private_key
+declare public_key
+declare target_port
+declare virtual_port
 
+readonly hidden_service_dir='/ssl/tor/hidden_service'
+readonly authorized_clients_dir="${hidden_service_dir}/authorized_clients"
+readonly clients_dir="${hidden_service_dir}/clients"
 readonly torrc='/etc/tor/torrc'
 
+
 if bashio::config.true 'hidden_services'; then
-    echo 'HiddenServiceDir /ssl/tor/hidden_service' >> "$torrc"
-    echo 'HiddenServiceVersion 3' >> "$torrc"
+    echo "HiddenServiceDir ${hidden_service_dir}" >> "$torrc"
+    
     for port in $(bashio::config 'ports'); do
         count=$(echo "${port}" | sed 's/[^:]//g'| awk '{ print length }')
         if [[ "${count}" == 0 ]]; then
@@ -46,8 +53,62 @@ if bashio::config.true 'hidden_services'; then
     done
 
     if bashio::config.true 'stealth'; then
+        # Following the documentation at:
+        # https://community.torproject.org/onion-services/advanced/client-auth/
         while read -r clientname; do
-            echo "HiddenServiceAuthorizeClient stealth $clientname" >> "$torrc"
+            # Generate key is they do not exist yet
+            if ! bashio::fs.file_exists "${authorized_clients_dir}/${clientname}.auth"
+            then 
+                key=$(openssl genpkey -algorithm x25519)
+
+                private_key=$(
+                    sed \
+                        -e '/----.*PRIVATE KEY----\|^[[:space:]]*$/d' \
+                        <<< "${key}" \
+                        | base64 -d \
+                        | tail -c 32 \
+                        | base32 \
+                        | sed 's/=//g'
+                )
+
+                # Public
+                public_key=$(
+                    openssl pkey -pubout \
+                    <<< "${key}" \
+                    | sed -e '/----.*PUBLIC KEY----\|^[[:space:]]*$/d' \
+                    | base64 -d \
+                    | tail -c 32 \
+                    | base32 \
+                    | sed 's/=//g'
+                )
+
+                # Create authorized client file
+                echo "descriptor:x25519:${public_key}" \
+                    > "${clients_dir}/${clientname}.auth"
+                echo "descriptor:x25519:${public_key}" \
+                    > "${authorized_clients_dir}/${clientname}.auth"
+
+                # Create private key file
+                echo "descriptor:x25519:${public_key}" \
+                    > "${clients_dir}/${clientname}.auth_private"
+
+                bashio::log.red
+                bashio::log.red
+                bashio::log.red "Created keys for ${clientname}!"
+                bashio::log.red
+                bashio::log.red "Keys are stored in:"
+                bashio::log.red "${clients_dir}"
+                bashio::log.red
+                bashio::log.red "Public key":
+                bashio::log.red "${public_key}"
+                bashio::log.red
+                bashio::log.red "Private key:"
+                bashio::log.red "${private_key}"
+                bashio::log.red
+                bashio::log.red 
+            else
+                bashio::log.info "Keys for ${clientname} already exists; skipping..."
+            fi
         done <<< "$(bashio::config 'client_names')"
     fi
 
