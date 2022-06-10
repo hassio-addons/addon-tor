899d3719619fb0fe6c1cc0930f2ab05e118ea7ef
Franck Nijhof <git@frenck.dev>
2022-06-10 20:31:38 +0200

    Simplify data directory creation

--- tor/rootfs/etc/cont-init.d/20-dirs.sh
+++ tor/rootfs/etc/cont-init.d/20-dirs.sh
@@ -4,9 +4,8 @@
 # Ensures necessary directories exists
 # ==============================================================================
 if ! bashio::fs.directory_exists '/ssl/tor'; then
-    mkdir -p /ssl/tor/hidden_service \
+    mkdir -p 
+        /ssl/tor/hidden_service/authorized_clients \
         || bashio::exit.nok 'Could not create tor data directory'
-    mkdir -p /ssl/tor/hidden_service/authorized_clients/ \
-        || bashio::exit.nok 'Could not create tor directory for client authorization'
     chmod -R 0700 /ssl/tor
 fi
