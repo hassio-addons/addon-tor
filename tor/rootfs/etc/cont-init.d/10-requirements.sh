109066320a50cc61cac5a28b5bdd71a34192bca2
Franck Nijhof <git@frenck.dev>
2022-06-10 19:58:20 +0200

    ⬆️ Upgrades add-on base image to 12.0.0 (#136)

--- tor/rootfs/etc/cont-init.d/10-requirements.sh
+++ tor/rootfs/etc/cont-init.d/10-requirements.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
 # ==============================================================================
 # Home Assistant Community Add-on: Tor
 # This files check if all user configuration requirements are met
--- tor/rootfs/etc/cont-init.d/50-tor-log.sh
+++ tor/rootfs/etc/cont-init.d/50-tor-log.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
 # ==============================================================================
 # Home Assistant Community Add-on: Tor
 # Sets the correct log level to the Tor configuration
